c42f702da3684ee0f916fe3f0cdae7eff8882b4f
gauravpatil159 <61248090+gauravpatil159@users.noreply.github.com>
2020-02-23 04:55:53 -0800

    Update min.c
    leave space after operators..

--- min.c
+++ min.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 int main()
 {
-int n,i,min;
+int n, i, min;
 int arr[100];
-printf("How many numbers you want to compare=");
-scanf("%d",&n);
-for(i=0;i<n;i++)
+printf("How many numbers you want to caompare=");
+scanf("%d", &n);
+for(i = 0;i < n; i++)
 {
     printf("Enter No");
-    scanf("%d",&arr[i]);
+    scanf("%d", &arr[i]);
 }
 min=arr[0];
-for(i=1;i<n;i++)
+for(i = 1; i < n; i++)
 {
-    if (min>arr[i])
-    min=arr[i];
+    if (min > arr[i])
+    min = arr[i];
     else
     {
-        min=min;
+        min = min;
     }
 
     
    
 }
-printf("Minimum no is %d",min);
+printf("Minimum no is %d", min);
 return 0;
 }
