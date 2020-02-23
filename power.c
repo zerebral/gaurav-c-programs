a5b43c8dad5cbd31773514d86d7ae01357bb3706
gauravpatil159 <61248090+gauravpatil159@users.noreply.github.com>
2020-02-23 04:59:02 -0800

    Update power.c
    leave space after operators

--- power.c
+++ power.c
@@ -2,21 +2,21 @@
 int pwr(int n, int m);
 int main()
 {
-    int n,m,ans;
+    int n , m , ans;
     printf("Enter number and it's power");
-    scanf("%d %d",&n,&m);
+    scanf("%d %d", &n, &m);
     ans=pwr(n,m);
-    printf("Answer=%d",ans);
+    printf("Answer=%d", ans);
     return 0;
 }
 
 
 int pwr(int n, int m)                /*function pwr use to get n raise to m using function*/
 {
-    int i,a;
-    a=n;
-    for(i=m;i>1;i--)
-    n=n*a;
+    int i, a;
+    a = n;
+    for(i = m; i > 1; i--)
+    n = n * a;
     return (n);
 
 }
