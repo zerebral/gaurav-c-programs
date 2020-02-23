88382ae2310ece554f95d66268fb3d897a5e607d
gauravpatil159 <61248090+gauravpatil159@users.noreply.github.com>
2020-02-22 20:33:25 -0800

    Update max.c
    to find max no in array

---
+++ max.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+int n,i,max;
+int arr[100];
+printf("How many numbers you want to caompare=");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    printf("Enter No");
+    scanf("%d",&arr[i]);
+}
+max=arr[0];
+for(i=1;i<n;i++)
+{
+    if (max<arr[i])
+    max=arr[i];
+    else
+    {
+        max=max;
+    }
+
+    
+   
+}
+printf("Maximum no is %d",max);
+}
