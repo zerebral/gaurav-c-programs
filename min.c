3c4441ad58ed6056058eb4116a856af439e6e01f
gauravpatil159 <61248090+gauravpatil159@users.noreply.github.com>
2020-02-22 20:12:32 -0800

    Update min.c
    To find minimum no is array of any size(Upto 100 here)

---
+++ min.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+int n,i,min;
+int arr[100];
+printf("How many numbers you want to caompare=");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    printf("Enter No");
+    scanf("%d",&arr[i]);
+}
+min=arr[0];
+for(i=1;i<n;i++)
+{
+    if (min>arr[i])
+    min=arr[i];
+    else
+    {
+        min=min;
+    }
+
+    
+   
+}
+printf("Minimum no is %d",min);
+}
