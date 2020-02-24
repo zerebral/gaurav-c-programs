a70fa778e681b39c6febb8bcaad5e72cae01521a
gauravpatil159 <61248090+gauravpatil159@users.noreply.github.com>
2020-02-23 20:25:39 -0800

    Update min.c
    Edit after review suggestion on max.c

--- arrays/min.c
+++ arrays/min.c
@@ -2,29 +2,29 @@
 int main()
 {
     int arr[10];
-    int n,m,i;
-    printf("Enter how much nos you have to compare =");
-    scanf("%d",&n);
+    int n, m, i;
+    printf("Enter how many numbers you have to compare ?");
+    scanf("%d", &n);
     
-    for(i=0 ; i < n; i++)
-    //if(n>0)
+    for(i = 0; i < n; i++)
+    
     {
-    printf("Enter numbers = ");
-    scanf("%d",&arr[i]);
+        printf("Enter numbers = ");
+        scanf("%d", &arr[i]);
     }
 
     i = 0;
     m = arr[0];
 
-    while (i<n)
+    while (i < n)
     {
         if(m > arr[i])
-        m = arr[i];
-        else
-        m=m;
+        {    
+            m = arr[i];
+        }
         i++;
     }
     
-    printf("Minimum number in array of %d numbers is = %d",n ,m);
+    printf("Minimum number in array of %d numbers is = %d", n, m);
     return 0;
 }
