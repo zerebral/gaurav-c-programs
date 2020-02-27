cfa1c0a048221321ae5e60b8c59f99730b9d202c
gauravpatil159 <61248090+gauravpatil159@users.noreply.github.com>
2020-02-26 18:04:32 -0800

    Update sum.c

--- arrays/sum.c
+++ arrays/sum.c
@@ -2,26 +2,26 @@
 int main()
 {
     int arr[100];
-    int n,m,i;
-    printf("Enter how much nos you have to add=");
-    scanf("%d",&n);
+    int n, m, i;
+    printf("Enter how many nos you have to add?");
+    scanf("%d", &n);
     
-    for(i=0 ; i < n; i++)
+    for(i = 0 ; i < n; i++)
     //if(n>0)
     {
     printf("Enter numbers =");
-    scanf("%d",&arr[i]);
+    scanf("%d", &arr[i]);
     }
 
     i = 0;
     m = 0;
 
-    while (i<n)
+    while (i < n)
     {
         m = m + arr[i];
         i++;
     }
     
-    printf("Sum 0f %d numbers is = %d",n ,m);
+    printf("Sum 0f %d numbers is = %d", n, m);
     return 0;
 }
