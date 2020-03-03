08610907f500bb30f560dbe5b702478500f4580e
gauravpatil159 <61248090+gauravpatil159@users.noreply.github.com>
2020-03-03 04:51:17 -0800

    Update student.c
    Finally find error ..

--- structures/student.c
+++ structures/student.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-#include <conio.h>
 
 struct Student
 {
@@ -9,55 +8,52 @@ struct Student
     char dept[10];
     char course[10];
     int yoj;
-}stu[450];
-
+};
+struct Student stu[450];
+int fun_1(struct Student stu[450], int );
 int main()
 {
     int n, i;
     
     printf("Enter number of students=");
     scanf("%d", &n);
-    struct Student stu[450];
+    
     for(i = 0; i < n; i++)
     {
         printf("Enter Roll No , Name of student\n");
         scanf("%d %s", &stu[i].roll_no, &stu[i].name);
-        //printf("Enter Department & Name of course\n");
-        //scanf("%s %s", &stu[i].dept, &stu[i].course);
+        printf("Enter Department & Name of course\n");
+        scanf("%s %s", &stu[i].dept, &stu[i].course);
         printf(" and year of joining\n");
         scanf("%d", &stu[i].yoj);
         
     }
     
     fun_1(stu, n);
-    getch();
+    return 0;
     
 
 }
 
-int fun_1(struct Student st[], int a)
+int fun_1(struct Student stu[450], int n)
 
 {
-    int i, y;
+    int i, y, z = 0;
     printf("Enter joining year for which students names required\n ");
     scanf("%d", &y);
     
-    printf("Name of students who join in %s\n", y);
+    printf("Name of students who join in %d\n", y);
     
-    for(i = 0; i < a; i++)
+    for(i = 0; i < n; i++)
 
     {
-        if(st[i].yoj == y)           //Here is the problem! with strucure member value how to use == operator 
+        if((stu[i].yoj) == y)
         {
-            printf("%s", st[i].name);
-            
+            printf("%s\n", stu[i].name);
+            z++;
         }  
 
     }
-    //printf("Total numbers of students join in %d are %d\n", *b, d);
-
-    
-
-
+    printf("Total numbers of students join in %d are %d\n", y, z);
 }
 
